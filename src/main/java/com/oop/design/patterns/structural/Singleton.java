cab71096ed5f39115907f7a2eff8b9acffcff7be
thoughtoinnovate <hitesh,thinktank@gmail.com>
2020-07-13 18:13:13 +0530

    Singleton Design Pattern

---
+++ src/main/java/com/oop/design/patterns/structural/Singleton.java
@@ -0,0 +1,41 @@
+package com.oop.design.patterns.structural;
+
+import java.io.Serializable;
+
+public class Singleton implements Serializable {
+
+	private static volatile Singleton INSTANCE = null;
+
+	private Singleton() {
+		// Private constructor
+
+		// Handling reflection scenario
+		if (INSTANCE != null) {
+			throw new RuntimeException("Already have instance created,Please use getInstance method.");
+		}
+	}
+
+	public static Singleton getInstance() {
+
+		if (null == INSTANCE) {
+
+			// multi-threading handling
+			synchronized (Singleton.class) {
+
+				if (null == INSTANCE) {
+					INSTANCE = new Singleton();
+				}
+			}
+		}
+		return INSTANCE;
+	}
+
+	/**
+	 * prevents duplicate instance in deserialization
+	 */
+	protected Singleton readResolve() {
+		return getInstance();
+
+	}
+
+}
